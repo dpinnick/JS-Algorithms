52792d0d070c27220d6fc1c06d121f67bef49c8f
dpinnick <dpinnick@gmail.com>
2017-10-30 11:40:44 -0700

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,35 +1,28 @@
-/* PALINDROME
-A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward, such as madam or racecar.
-Sentence-length palindromes may be written when allowances are made for adjustments to capital letters, punctuation, and word dividers,
-such as "A man, a plan, a canal, Panama!", "Was it a car or a cat I saw?" or "No 'x' in Nixon".
-
-WIKIPEDIA: https://en.wikipedia.org/wiki/Palindrome
-*/
-
 // CHECK FOR PALINDROMES
 // Returns: true or false.
 // Useage: isPalindrome("Was it a car or a cat I saw");// true
-// Useage: isPalindrome("11012321011");// true
+// Useage: isPalindrome("11011");// true
 
-
-function isPalindrome(str) {// ES5 LONG
+// ES5 LONG
+function isPalindrome(str) {
     var regex = /[\W_]/g;
     str = str.toLowerCase().replace(regex, '');
     var copy = str.split('').reverse().join('');
     return copy == str;
 }
-
-function isPalindrome(str) {// ES5 SHORT
+// ES5 SHORT
+function isPalindrome(str) {
     return (str = str.toLowerCase().replace(/[\W_]/g, '')) == str.split('').reverse().join('');
 }
 
-const isPalindrome = str => {// ECMA6 LONG
+// ECMA6 LONG
+const isPalindrome = str => {
     const regex = /[\W_]/g;
     str = str.toLowerCase().replace(regex, '');
     const copy = str.split('').reverse().join('');
     return copy == str;
 };
-
-const isPalindrome = str => {// ECMA6 SHORT
+// ECMA6 SHORT
+const isPalindrome = str => {
     return (str = str.toLowerCase().replace(/[\W_]/g, '')) == str.split('').reverse().join('');
 };
