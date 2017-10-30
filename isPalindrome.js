753d219e1652ef68226e30c34b67095b071f1aa8
dpinnick <dpinnick@gmail.com>
2017-10-30 11:51:40 -0700

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -3,26 +3,26 @@
 // Useage: isPalindrome("Was it a car or a cat I saw");// true
 // Useage: isPalindrome("11011");// true
 
-// ES5 LONG
+// ES5 long
 function isPalindrome(str) {
     var regex = /[\W_]/g;
     str = str.toLowerCase().replace(regex, '');
     var copy = str.split('').reverse().join('');
     return copy == str;
 }
-// ES5 SHORT
+// ES5 short
 function isPalindrome(str) {
     return (str = str.toLowerCase().replace(/[\W_]/g, '')) == str.split('').reverse().join('');
 }
 
-// ECMA6 LONG
+// ES6 long
 const isPalindrome = str => {
     const regex = /[\W_]/g;
     str = str.toLowerCase().replace(regex, '');
     const copy = str.split('').reverse().join('');
     return copy == str;
 };
-// ECMA6 SHORT
+// ES6 short
 const isPalindrome = str => {
     return (str = str.toLowerCase().replace(/[\W_]/g, '')) == str.split('').reverse().join('');
 };
